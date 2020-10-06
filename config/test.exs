6db55ba80e8ad641e565eee6b454785dbd7712eb
Ralph Effting <ralfting@gmail.com>
2020-10-06 14:06:03 -0300

    Add postgres

--- config/test.exs
+++ config/test.exs
@@ -8,3 +8,16 @@ config :wise_path, WisePathWeb.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+# Configure the database for local testing
+config :wise_path, WisePath.Repo,
+  database: "wise_path_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# Configure the database for GitHub Actions
+if System.get_env("GITHUB_ACTIONS") do
+  config :wise_path, App.Repo,
+    username: "postgres",
+    password: "postgres"
+end
